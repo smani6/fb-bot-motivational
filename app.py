5c65dbd03aa17fbe66bf3ce35944a5cd0a83c6ef
smani6 <ms@gametop.com>
2017-04-15 00:06:19 +0800

    copied code from main.py to app.py

--- app.py
+++ app.py
@@ -8,15 +8,19 @@ app = Flask(__name__)
 # by the Facebook App that will be created.
 PAT = ''
 
-@app.route('/', methods=['GET'])
-def handle_verification():
-    print "Handling Verification."
-    if request.args.get('hub.verify_token') == 'my_voice_is_my_password_verify_me':
-        print "Verification successful!"
-        return request.args.get('hub.challenge')
-    else:
-        print "Verification failed!"
-        return 'Error, wrong validation token'
+@app.route("/")
+def main_function():
+	return "Hi Welcome"
+	
+# @app.route('/', methods=['GET'])
+# def handle_verification():
+#     print "Handling Verification."
+#     if request.args.get('hub.verify_token') == 'my_voice_is_my_password_verify_me':
+#         print "Verification successful!"
+#         return request.args.get('hub.challenge')
+#     else:
+#         print "Verification failed!"
+#         return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
 def handle_messages():
